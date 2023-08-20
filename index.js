a7b338e7336b86405f7ed945fc1565f4be2009e4
psobrakseaton <28573875+psobrakseaton@users.noreply.github.com>
2023-08-20 10:34:35 +0200

    add baffle.js to splash text

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+let baffleSplash = baffle('.splashText');
+
+baffleSplash.set({
+   characters: '░▓▓ >▓█/▒ /<░▒▒ ▒░█ ▒█<▒▓ ▓▓▒▓ ▒▒░ ▒█▓█ █▒░░',
+    speed: 100
+})
+  .start()
+  .reveal(1000);
